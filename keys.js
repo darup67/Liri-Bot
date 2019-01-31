fea104dbcb2b24c408d2ce4f186992adc4fd2e81
Dhruv Patel <darup67@gmail.com>
2019-01-30 20:10:48 -0500

    liri-bot

--- keys.js
+++ keys.js
@@ -2,5 +2,7 @@ console.log('this is loaded');
 
 exports.spotify = {
     id: process.env.SPOTIFY_ID,
-    secret: process.env.SPOTIFY_SECRET
+    "829abbe7bf494c5dbfa42445a1507805"
+    secret: process.env.SPOTIFY_SECRET,
+    "1776d4f74d6c4706a660523face159a7"
 };
